c781e9253b66bb80731016fdafeaed33b4cd4c23
sorodriguezz <s.rodriguezzapata01@gmail.com>
2025-10-25 00:02:44 -0300

    Actualizar la versión a 1.2.10 en package.json y corregir la gestión de propiedades en springConfigServerUseCase para incluir correctamente la propiedad 'data'.

--- src/use-case/spring-config-server.use-case.ts
+++ src/use-case/spring-config-server.use-case.ts
@@ -21,10 +21,16 @@ export const springConfigServerUseCase = async (
   const response = await axios.get(objAxios.url!, objAxios);
 
   const properties: Record<string, any> = {};
+
   if (response.data.propertySources) {
     response.data.propertySources.forEach((source: any) => {
       Object.assign(properties, source.source);
     });
+  } else if (response.data.source) {
+    Object.assign(properties, response.data.source);
+  } else {
+    const { name, profiles, label, version, state, ...rest } = response.data;
+    Object.assign(properties, rest);
   }
 
   return { data: properties };
